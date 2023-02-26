9aa9eb919712cc1ec035f88c7078beda20c60302
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-02-26 08:14:12 +0000

    feat: default kwarg

--- a_sync/decorator.py
+++ a_sync/decorator.py
@@ -1,6 +1,6 @@
 
 import functools
-from typing import Callable, TypeVar
+from typing import Callable, TypeVar, Literal, Optional
 
 from typing_extensions import ParamSpec  # type: ignore
 
@@ -10,25 +10,36 @@ P = ParamSpec("P")
 T = TypeVar("T")
 
 
-def a_sync(coro_fn: Callable[P, T]) -> Callable[P, T]:  # type: ignore
+def a_sync(default: Optional[Literal['sync','async']] = None) -> Callable[[Callable[P, T]], Callable[P, T]]:
     f"""
     A coroutine function decorated with this decorator can be called as a sync function by passing a boolean value for any of these kwargs: {_helpers._flag_name_options}
     """
+    if default not in ['async', 'sync', None]:
+        if callable(default):
+            return a_sync()(default)
+        raise ValueError(f"'default' must be either 'sync', 'async', or None. You passed {default}.")
     
-    _helpers._validate_wrapped_fn(coro_fn)
+    def a_sync_deco(coro_fn: Callable[P, T]) -> Callable[P, T]:  # type: ignore
+    
+        _helpers._validate_wrapped_fn(coro_fn)
 
-    @functools.wraps(coro_fn)
-    def a_sync_wrap(*args: P.args, **kwargs: P.kwargs) -> T:  # type: ignore
-        # If a flag was specified in the kwargs, we will defer to it.
-        for flag in _helpers._flag_name_options:
-            if flag in kwargs:
-                val = kwargs.pop(flag)
-                if not isinstance(val, bool):
-                    raise TypeError(f"'{flag}' must be boolean. You passed {val}.")
-                return _helpers._await_if_sync(  # type: ignore
-                    coro_fn(*args, **kwargs),
-                    val if flag == 'sync' else not val
-                )
-        # No flag specified in the kwargs, we will just return the awaitable.
-        return coro_fn(*args, **kwargs)
-    return a_sync_wrap
+        @functools.wraps(coro_fn)
+        def a_sync_wrap(*args: P.args, **kwargs: P.kwargs) -> T:  # type: ignore
+            # If a flag was specified in the kwargs, we will defer to it.
+            for flag in _helpers._flag_name_options:
+                if flag in kwargs:
+                    val = kwargs.pop(flag)
+                    if not isinstance(val, bool):
+                        raise TypeError(f"'{flag}' must be boolean. You passed {val}.")
+                    return _helpers._await_if_sync(
+                        coro_fn(*args, **kwargs),
+                        val if flag == 'sync' else not val
+                    )
+                
+            # No flag specified in the kwargs, we will defer to 'default'.
+            return _helpers._await_if_sync(
+                coro_fn(*args, **kwargs),
+                True if default == 'sync' else False
+            )
+        return a_sync_wrap
+    return a_sync_deco
