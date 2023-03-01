1d0ea8c811ce100c09306930bc7194a99516cbdb
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-03-01 07:38:19 +0000

    feat: improve type hinting for property decorators

--- a_sync/property.py
+++ a_sync/property.py
@@ -3,10 +3,10 @@ import asyncio
 
 import async_property as ap
 
+from a_sync import config
 from a_sync._typing import *
+from a_sync.modified import Modified
 from a_sync.modifiers import Modifiers
-
-T = TypeVar('T')
 
 
 class PropertyDescriptor(Modified[T]):
@@ -20,51 +20,55 @@ class AsyncPropertyDescriptor(PropertyDescriptor[T], ap.base.AsyncPropertyDescri
 class AsyncCachedPropertyDescriptor(PropertyDescriptor[T], ap.cached.AsyncCachedPropertyDescriptor):
     pass
 
-
 @overload
 def a_sync_property(
     func: Literal[None] = None,
-    **modifiers: Modifiers,
-) -> Callable[[Callable[..., T]], AsyncPropertyDescriptor]:...
+    default: Literal['sync', 'async', None] = config.DEFAULT_MODE,
+    **modifiers: ModifierKwargs,
+) -> Callable[[Callable[..., T]], AsyncPropertyDescriptor[T]]:...
     
 @overload
 def a_sync_property(
     func: Callable[..., T] = None,
-    **modifiers: Modifiers,
-) -> AsyncPropertyDescriptor:...
+    default: Literal['sync', 'async', None] = config.DEFAULT_MODE,
+    **modifiers: ModifierKwargs,
+) -> AsyncPropertyDescriptor[T]:...
     
 def a_sync_property(
     func: Optional[Callable[..., T]] = None,
-    **modifiers: Modifiers,
+    default: Literal['sync', 'async', None] = config.DEFAULT_MODE,
+    **modifiers: ModifierKwargs,
 ) -> Union[
-    AsyncPropertyDescriptor,
-    Callable[[Callable[..., T]], AsyncPropertyDescriptor],
+    AsyncPropertyDescriptor[T],
+    Callable[[Callable[..., T]], AsyncPropertyDescriptor[T]],
 ]:
-    def modifier_wrap(func) -> AsyncPropertyDescriptor:
+    def modifier_wrap(func) -> AsyncPropertyDescriptor[T]:
         assert asyncio.iscoroutinefunction(func), 'Can only use with async def'
-        return AsyncPropertyDescriptor(func, **modifiers)
+        return AsyncPropertyDescriptor(func, modifiers=Modifiers(modifiers))
     return modifier_wrap if func is None else modifier_wrap(func)
     
 @overload
 def a_sync_cached_property(
     func: Literal[None] = None,
-    **modifiers: Modifiers,
-) -> Callable[[Callable[..., T]], AsyncCachedPropertyDescriptor]:...
+    default: Literal['sync', 'async', None] = config.DEFAULT_MODE,
+    **modifiers: ModifierKwargs,
+) -> Callable[[Callable[..., T]], AsyncCachedPropertyDescriptor[T]]:...
     
 @overload
 def a_sync_cached_property(
     func: Callable[..., T] = None,
-    **modifiers: Modifiers,
-) -> AsyncCachedPropertyDescriptor:...
+    default: Literal['sync', 'async', None] = config.DEFAULT_MODE,
+    **modifiers: ModifierKwargs,
+) -> AsyncCachedPropertyDescriptor[T]:...
     
 def a_sync_cached_property(
     func: Optional[Callable[..., T]] = None,
-    **modifiers: Modifiers,
+    **modifiers: ModifierKwargs,
 ) -> Union[
-    AsyncCachedPropertyDescriptor,
-    Callable[[Callable[..., T]], AsyncCachedPropertyDescriptor],
+    AsyncCachedPropertyDescriptor[T],
+    Callable[[Callable[..., T]], AsyncCachedPropertyDescriptor[T]],
 ]:
-    def modifier_wrap(func) -> AsyncCachedPropertyDescriptor:
+    def modifier_wrap(func) -> AsyncCachedPropertyDescriptor[T]:
         assert asyncio.iscoroutinefunction(func), 'Can only use with async def'
-        return AsyncCachedPropertyDescriptor(func, **modifiers)
+        return AsyncCachedPropertyDescriptor(func, modifiers=Modifiers(modifiers))
     return modifier_wrap if func is None else modifier_wrap(func)
