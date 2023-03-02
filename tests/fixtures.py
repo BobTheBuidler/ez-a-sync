31621c6bf7adeff66e23911463232f9b92e40ad6
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-03-02 08:13:18 +0000

    feat(test): test running location

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,5 +1,7 @@
 
 import asyncio
+import time
+from threading import current_thread, main_thread
 
 import pytest
 
@@ -16,15 +18,61 @@ class TestClass(ASyncBase):
         self.sync = sync
     
     async def test_fn(self) -> int:
+        if self.sync == False:
+            assert main_thread() == current_thread(), f'This should be running on an event loop in the main thread.'
+        elif self.sync == True:
+            assert main_thread() == current_thread(), f'This should be awaited in the main thread'
         return self.v
     
     @a_sync.aka.property
     async def test_property(self) -> int:
+        if self.sync == False:
+            assert main_thread() == current_thread(), f'This should be running on an event loop in the main thread.'
+        elif self.sync == True:
+            assert main_thread() == current_thread(), f'This should be awaited in the main thread'
         return self.v * 2
     
     @a_sync.alias.cached_property
     async def test_cached_property(self) -> int:
+        if self.sync == False:
+            assert main_thread() == current_thread(), f'This should be running on an event loop in the main thread.'
+        elif self.sync == True:
+            assert main_thread() == current_thread(), f'This should be awaited in the main thread'
         await asyncio.sleep(2)
+        return self.v * 3
+
+class TestSync(ASyncBase):
+    main = main_thread()
+    def __init__(self, v: int, sync: bool):
+        self.v = v
+        self.sync = sync
+    
+    def test_fn(self) -> int:
+        # Sync bound methods are actually async functions that are run in an executor and awaited
+        if self.sync == False:
+            assert main_thread() == self.main
+            assert main_thread() is not current_thread(), f'This should be running in an executor, not the main thread.'
+        elif self.sync == True:
+            assert main_thread() is not current_thread(), f'This should be running synchronously in the main thread'
+        return self.v
+    
+    @a_sync.aka.property
+    def test_property(self) -> int:
+        if self.sync == False:
+            assert main_thread() is not current_thread(), f'This should be running in an executor, not the main thread.'
+        elif self.sync == True:
+            # Sync properties are actually async functions that are run in an executor and awaited
+            assert main_thread() is not current_thread(), f'This should be running in an executor, not the main thread.'
+        return self.v * 2
+    
+    @a_sync.alias.cached_property
+    def test_cached_property(self) -> int:
+        if self.sync == False:
+            assert main_thread() is not current_thread(), f'This should be running in an executor, not the main thread.'
+        elif self.sync == True:
+            # Sync properties are actually async functions that are run in an executor and awaited
+            assert main_thread() is not current_thread(), f'This should be running in an executor, not the main thread.'
+        time.sleep(2)
         return self.v * 3
 
 class TestInheritor(TestClass):
@@ -34,7 +82,9 @@ class TestMeta(TestClass, metaclass=ASyncMeta):
     pass
 
 class TestSingleton(ASyncGenericSingleton, TestClass):
+    runs_per_minute = 100
     pass
 
 class TestSingletonMeta(TestClass, metaclass=ASyncSingletonMeta):
+    semaphore = 1
     pass
