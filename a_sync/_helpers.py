686f155a11b82ffaef6dde941a5e3eac103b8ed3
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2023-05-31 13:45:09 -0400

    fix: no event loop in current thread

--- a_sync/_helpers.py
+++ a_sync/_helpers.py
@@ -12,6 +12,13 @@ from a_sync import _flags
 from a_sync._typing import *
 
 
+def get_event_loop() -> asyncio.BaseEventLoop:
+    try:
+        return asyncio.get_event_loop()
+    except RuntimeError:
+        asyncio.set_event_loop(asyncio.new_event_loop())
+        return asyncio.get_event_loop()
+    
 def _validate_wrapped_fn(fn: Callable) -> None:
     """Ensures 'fn' is an appropriate function for wrapping with a_sync."""
     if isinstance(fn, (AsyncPropertyDescriptor, AsyncCachedPropertyDescriptor)):
@@ -27,7 +34,7 @@ running_event_loop_msg = f"You may want to make this an async function by settin
 
 def _await(awaitable: Awaitable[T]) -> T:
     try:
-        return asyncio.get_event_loop().run_until_complete(awaitable)
+        return get_event_loop().run_until_complete(awaitable)
     except RuntimeError as e:
         if str(e) == "This event loop is already running":
             raise RuntimeError(str(e), running_event_loop_msg)
@@ -36,5 +43,5 @@ def _await(awaitable: Awaitable[T]) -> T:
 def _asyncify(func: SyncFn[P, T], executor: Executor) -> CoroFn[P, T]:
     @functools.wraps(func)
     async def _asyncify_wrap(*args: P.args, **kwargs: P.kwargs) -> T:
-        return await asyncio.get_event_loop().run_in_executor(executor, func, *args, **kwargs)
+        return await get_event_loop().run_in_executor(executor, func, *args, **kwargs)
     return _asyncify_wrap
