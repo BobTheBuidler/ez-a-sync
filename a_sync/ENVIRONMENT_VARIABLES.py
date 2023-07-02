05cabec6ffa8b2a64c1f0b84e91b0d993db4d226
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-07-02 23:31:16 +0000

    feat: implement typed-envs

--- a_sync/ENVIRONMENT_VARIABLES.py
+++ a_sync/ENVIRONMENT_VARIABLES.py
@@ -1,8 +1,12 @@
 
-import os
+from typed_envs import EnvVarFactory
+
+envs = EnvVarFactory("EZASYNC")
 
 # We have some envs here to help you debug your custom class implementations
+
 # If you're only interested in debugging a specific class, set this to the class name
-DEBUG_CLASS_NAME = os.environ.get("EZASYNC_DEBUG_CLASS_NAME") 
+DEBUG_CLASS_NAME = envs.create_env("DEBUG_CLASS_NAME", str, default='', verbose=False) 
+
 # Set this to enable debug mode on all classes
-DEBUG_MODE = bool(os.environ.get("EZASYNC_DEBUG_MODE", DEBUG_CLASS_NAME))
+DEBUG_MODE = envs.create_env("DEBUG_MODE", bool, default=DEBUG_CLASS_NAME, verbose=False)
