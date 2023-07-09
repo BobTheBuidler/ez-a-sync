18bb04b6868d263430eb00a12ab9cb35c7902ed6
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-07-09 22:04:54 +0000

    feat: Event.loop convenience method to get loop in later py versions

--- a_sync/primitives/locks/event.py
+++ a_sync/primitives/locks/event.py
@@ -1,6 +1,9 @@
 
 import asyncio
+from functools import cached_property
+
 from a_sync.primitives._debug import _DebugDaemonMixin
+
 
 class Event(asyncio.Event, _DebugDaemonMixin):
     """asyncio.Event but with some additional debug logging to help detect deadlocks."""
@@ -9,6 +12,10 @@ class Event(asyncio.Event, _DebugDaemonMixin):
             return True
         self._ensure_debug_daemon()
         return await super().wait()
+    
+    @cached_property
+    def loop(self) -> asyncio.AbstractEventLoop:
+        return self._loop or asyncio.get_event_loop()
         
     async def _debug_daemon(self) -> None:
         while not self.is_set():
