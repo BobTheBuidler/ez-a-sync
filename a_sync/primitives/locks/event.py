4c00fe8aceec4a507ba49e301709c696773a70cb
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-07-10 00:37:34 +0000

    feat: settable Event debugger interval

--- a_sync/primitives/locks/event.py
+++ a_sync/primitives/locks/event.py
@@ -1,12 +1,17 @@
 
 import asyncio
 from functools import cached_property
+from typing import Optional
 
 from a_sync.primitives._debug import _DebugDaemonMixin
 
 
 class Event(asyncio.Event, _DebugDaemonMixin):
     """asyncio.Event but with some additional debug logging to help detect deadlocks."""
+    def __init__(self, debug_daemon_interval: int = 300, *, loop: Optional[asyncio.AbstractEventLoop] = None):
+        super().__init__(loop=loop)
+        self._loop = self._loop or asyncio.get_event_loop()
+        self._debug_daemon_interval = debug_daemon_interval
     async def wait(self) -> bool:
         if self.is_set():
             return True
@@ -15,9 +20,10 @@ class Event(asyncio.Event, _DebugDaemonMixin):
     
     @cached_property
     def loop(self) -> asyncio.AbstractEventLoop:
-        return self._loop or asyncio.get_event_loop()
+        return 
         
     async def _debug_daemon(self) -> None:
         while not self.is_set():
-            self.logger.debug(f"Waiting for {self}")
-            await asyncio.sleep(60)
+            await asyncio.sleep(self._debug_daemon_interval)
+            if not self.is_set():
+                self.logger.debug(f"Waiting for {self}")
