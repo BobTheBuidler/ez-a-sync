5eca7379746c1c0eb3e96ce2e2c55a0bbf08de77
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-09-11 14:02:40 +0000

    feat: a_sync.any and a_sync.all utils

--- a_sync/__init__.py
+++ a_sync/__init__.py
@@ -5,7 +5,7 @@ from a_sync.decorator import a_sync
 from a_sync.modifiers.semaphores import apply_semaphore
 from a_sync.primitives import *
 from a_sync.singleton import ASyncGenericSingleton
-from a_sync.utils import as_yielded
+from a_sync.utils import all, any, as_yielded
 
 # I alias the aliases for your convenience.
 # I prefer "aka" but its meaning is not intuitive when reading code so I created both aliases for you to choose from.
