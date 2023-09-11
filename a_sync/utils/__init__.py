f6d1b5e3430f0ac7ab6464b9801fe42bc8bf624d
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2023-09-11 10:03:08 -0400

    feat: a_sync.any and a_sync.all utils (#63)

--- a_sync/utils/__init__.py
+++ a_sync/utils/__init__.py
@@ -1,2 +1,23 @@
+import asyncio
+
 from a_sync.utils.iterators import (as_yielded, exhaust_iterator,
                                     exhaust_iterators)
+
+
+async def any(*awaitables) -> bool:
+    futs = [asyncio.ensure_future(a) for a in awaitables]
+    for fut in asyncio.as_completed(futs):
+        if bool(await fut):
+            for fut in futs:
+                fut.cancel()
+            return True
+    return False
+    
+async def all(*awaitables) -> bool:
+    futs = [asyncio.ensure_future(a) for a in awaitables]
+    for fut in asyncio.as_completed(awaitables):
+        if not bool(await fut):
+            for fut in futs:
+                fut.cancel()
+            return False
+    return True
