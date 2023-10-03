8760c17457c8a641fa44cf64894683be15075d01
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2023-10-03 13:55:53 -0400

    fix: missing import

--- a_sync/future.py
+++ a_sync/future.py
@@ -2,7 +2,7 @@
 
 import asyncio
 from decimal import Decimal
-from functools import wraps
+from functools import partial, wraps
 from typing import (Any, Awaitable, Callable, List, Set, TypeVar, Union,
                     overload)
 
