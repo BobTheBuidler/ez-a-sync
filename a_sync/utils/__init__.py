b39cbcebfafae346a09bbe136a6fc4bf9e7f4b4a
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-10-07 21:27:02 +0000

    feat: mapping utils

--- a_sync/utils/__init__.py
+++ a_sync/utils/__init__.py
@@ -1,8 +1,19 @@
 import asyncio
-
+from typing import Awaitable, Dict, Iterator, Mapping, Tuple, TypeVar, List, Callable
+from typing_extensions import ParamSpec
 from a_sync.utils.iterators import (as_yielded, exhaust_iterator,
                                     exhaust_iterators)
 
+T = TypeVar("T")
+KT = TypeVar("KT")
+VT = TypeVar("VT")
+P = ParamSpec("P")
+
+__all__ = [
+    "all",
+    "any",
+    "as_yielded",
+]
 
 async def any(*awaitables) -> bool:
     futs = [asyncio.ensure_future(a) for a in awaitables]
@@ -21,3 +32,17 @@ async def all(*awaitables) -> bool:
                 fut.cancel()
             return False
     return True
+
+async def gather_mapping(mapping: Mapping[KT, Awaitable[VT]]) -> Dict[KT, VT]:
+    results = {k: None for k in mapping.keys()}  # return data in same order
+    async for k, v in as_completed_mapping(mapping):
+        results[k] = v
+    return results
+
+def as_completed_mapping(mapping: Mapping[KT, Awaitable[VT]]) -> Iterator["asyncio.Task[Tuple[KT, VT]]"]:
+    return asyncio.as_completed([__as_completed_wrap(k, v) for k, v in mapping.items()])
+
+async def __as_completed_wrap(k: KT, v: Awaitable[VT]) -> VT:
+    return k, await v
+
+    
