2614be4d2b2df044c98a42ee5b1c432b87a6f621
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-10-10 04:20:13 +0000

    feat: more robust .gather and .as_completed

--- a_sync/utils/__init__.py
+++ a_sync/utils/__init__.py
@@ -1,6 +1,11 @@
 import asyncio
-from typing import Awaitable, Dict, Iterator, Mapping, Tuple, TypeVar, List, Callable
+from typing import (Any, AsyncIterator, Awaitable, Dict, Iterable, Iterator,
+                    Mapping, Optional, Tuple, TypeVar, Union, overload)
+
+from tqdm.asyncio import tqdm_asyncio
 from typing_extensions import ParamSpec
+
+from a_sync.iter import ASyncIterator
 from a_sync.utils.iterators import (as_yielded, exhaust_iterator,
                                     exhaust_iterators)
 
@@ -33,16 +38,36 @@ async def all(*awaitables) -> bool:
             return False
     return True
 
-async def gather_mapping(mapping: Mapping[KT, Awaitable[VT]]) -> Dict[KT, VT]:
+async def gather_mapping(mapping: Mapping[KT, Awaitable[VT]], tqdm: bool = False, **tqdm_kwargs: Any) -> Dict[KT, VT]:
     results = {k: None for k in mapping.keys()}  # return data in same order
-    async for k, v in as_completed_mapping(mapping):
+    async for k, v in as_completed_mapping(mapping, tqdm=tqdm, **tqdm_kwargs):
         results[k] = v
     return results
 
-def as_completed_mapping(mapping: Mapping[KT, Awaitable[VT]]) -> Iterator["asyncio.Task[Tuple[KT, VT]]"]:
-    return asyncio.as_completed([__as_completed_wrap(k, v) for k, v in mapping.items()])
+def as_completed(fs: Iterable[Awaitable[T]], *, timeout: Optional[float] = None, aiter: bool = False, tqdm: bool = False, **tqdm_kwargs: Any):
+    return (
+        as_completed_mapping(fs) if isinstance(fs, Mapping)
+        else ASyncIterator.wrap(__yield_as_completed(fs, tqdm=tqdm, **tqdm_kwargs)) if aiter 
+        else tqdm_asyncio.as_completed(fs, timeout=timeout, **tqdm_kwargs) if tqdm 
+        else asyncio.as_completed(fs, timeout=timeout)
+    )
 
-async def __as_completed_wrap(k: KT, v: Awaitable[VT]) -> VT:
+@overload
+def as_completed_mapping(mapping: Mapping[KT, Awaitable[VT]], *, timeout: Optional[float] = None, aiter = True, tqdm: bool, **tqdm_kwargs: Any) -> ASyncIterator[Tuple[KT, VT]]:...
+@overload
+def as_completed_mapping(mapping: Mapping[KT, Awaitable[VT]], *, timeout: Optional[float] = None, aiter = False, tqdm: bool, **tqdm_kwargs: Any) -> Iterator[Awaitable[Tuple[KT, VT]]]:...
+def as_completed_mapping(mapping: Mapping[KT, Awaitable[VT]], *, timeout: Optional[float] = None, aiter: bool = False, tqdm: bool = False, **tqdm_kwargs: Any) -> Union[Iterator[Awaitable[Tuple[KT, VT]]], ASyncIterator[Tuple[KT, VT]]]:
+    return as_completed([__mapping_wrap(k, v) for k, v in mapping.items()], timeout=timeout, aiter=aiter, tqdm=tqdm, **tqdm_kwargs)
+
+async def __yield_as_completed(futs: Iterable[Awaitable[T]], *, timeout: Optional[float] = None, tqdm: bool = False, **tqdm_kwargs: Any) -> AsyncIterator[T]:
+    if tqdm:
+        for fut in tqdm_asyncio.as_completed(futs, timeout=timeout, **tqdm_kwargs):
+            yield await fut
+    else:
+        for fut in asyncio.as_completed(futs, timeout=timeout):
+            yield await fut
+        
+async def __mapping_wrap(k: KT, v: Awaitable[VT]) -> VT:
     return k, await v
 
     
