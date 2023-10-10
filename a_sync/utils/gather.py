37b7aea232d29787059c6d2b635cc9ebed8d56a0
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-10-10 04:20:13 +0000

    fix: gather_mapping

--- a_sync/utils/gather.py
+++ a_sync/utils/gather.py
@@ -12,13 +12,15 @@ VT = TypeVar('VT')
 
 async def gather(*awaitables: Awaitable[T], return_exceptions: bool = False, tqdm: bool = False, **tqdm_kwargs: Any) -> List[T]:
     return await (
-        gather_mapping(awaitables[0], return_exceptions=return_exceptions, tqdm=tqdm, **tqdm_kwargs) if len(awaitables) == 1 and isinstance(awaitables[0], Mapping)
+        gather_mapping(awaitables[0], return_exceptions=return_exceptions, tqdm=tqdm, **tqdm_kwargs) if _is_mapping(awaitables)
         else tqdm_asyncio.gather(*awaitables, return_exceptions=return_exceptions, **tqdm_kwargs) if tqdm
         else asyncio.gather(*awaitables, return_exceptions=return_exceptions)
     )
     
 async def gather_mapping(mapping: Mapping[KT, Awaitable[VT]], return_exceptions: bool = False, tqdm: bool = False, **tqdm_kwargs: Any) -> Dict[KT, VT]:
     results = {k: None for k in mapping.keys()}  # return data in same order
-    async for k, v in as_completed_mapping(mapping, return_exceptions=return_exceptions, tqdm=tqdm, **tqdm_kwargs):
+    async for k, v in as_completed_mapping(mapping, return_exceptions=return_exceptions, aiter=True, tqdm=tqdm, **tqdm_kwargs):
         results[k] = v
     return results
+
+_is_mapping = lambda awaitables: len(awaitables) == 1 and isinstance(awaitables[0], Mapping)
