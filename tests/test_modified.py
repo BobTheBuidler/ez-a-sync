9d2d97e399aa5d6a1c01cfbcd3a0913520d8f916
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2023-10-16 16:06:54 -0400

    fix: no attribute __name__

---
+++ tests/test_modified.py
@@ -0,0 +1,16 @@
+
+import a_sync
+
+@a_sync.a_sync
+def sync_def():
+    pass
+
+@a_sync.a_sync
+async def async_def():
+    pass
+
+def test_sync_def_repr():
+    assert sync_def.__name__ == "sync_def"
+
+def test_async_def_repr():
+    assert async_def.__name__ == "async_def"
