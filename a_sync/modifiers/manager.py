cb8e06e43ee607c0ac56a9f095ff8b2aa0fe9b53
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-02-11 23:19:11 +0000

    Revert "chore: fix mypy errs"
    This reverts commit 8bd28f63801db7c463fd1286f77fa868d47c6d65.

--- a_sync/modifiers/manager.py
+++ a_sync/modifiers/manager.py
@@ -70,11 +70,11 @@ class ModifierManager(Dict[str, Any]):
         return sync_modifier_wrap
     
     # Dictionary api
-    def items(self) -> dict_items[str, Any]:
+    def items(self) -> ItemsView[str, Any]:
         return self._modifiers.items()
-    def keys(self) -> dict_keys[str]:
+    def keys(self) -> KeysView[str]:
         return self._modifiers.keys()
-    def values(self) -> dict_values[Any]:
+    def values(self) -> ValuesView[Any]:
         return self._modifiers.values()
     def __contains__(self, key: str) -> bool:  # type: ignore [override]
         return key in self._modifiers
