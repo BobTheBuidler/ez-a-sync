2b2d69694b844c5489fd7fd9db43035c1f622d8f
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-02-13 09:47:55 -0500

    chore: refactor map

--- a_sync/utils/map.py
+++ a_sync/utils/map.py
@@ -1,13 +1,8 @@
 
 from a_sync._typing import *
-from a_sync.iter import ASyncIterator
 
 MappingFn = Callable[Concatenate[K, P], Awaitable[V]]
 
-@overload
-def map(coro_fn: MappingFn[K, P, V], *iterables: AnyIterable[K], yields: Literal['keys'], **coro_fn_kwargs: P.kwargs) -> ASyncIterator[K]:...
-@overload
-def map(coro_fn: MappingFn[K, P, V], *iterables: AnyIterable[K], yields: Literal['both'], **coro_fn_kwargs: P.kwargs) -> ASyncIterator[Tuple[K, V]]:...
-def map(coro_fn: MappingFn[K, P, V], *iterables: AnyIterable[K], yields: Literal['keys', 'both'] = 'both', **coro_fn_kwargs: P.kwargs) -> Union[ASyncIterator[K], ASyncIterator[Tuple[K, V]]]:
+def map(coro_fn: MappingFn[K, P, V], *iterables: AnyIterable[K], **coro_fn_kwargs: P.kwargs) -> TaskMapping[K, V]:
     from a_sync.task import TaskMapping
-    return ASyncIterator.wrap(TaskMapping(coro_fn, **coro_fn_kwargs).map(*iterables, yields=yields))
+    return TaskMapping(coro_fn, *iterables, **coro_fn_kwargs)
