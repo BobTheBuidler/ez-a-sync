0260e9e93c945bc33b576c3e3970186e78bc38c1
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-02-13 09:59:01 -0500

    Delete a_sync/utils/map.py

--- a_sync/utils/map.py
+++
@@ -1,8 +0,0 @@
-
-from a_sync._typing import *
-
-MappingFn = Callable[Concatenate[K, P], Awaitable[V]]
-
-def map(coro_fn: MappingFn[K, P, V], *iterables: AnyIterable[K], **coro_fn_kwargs: P.kwargs) -> TaskMapping[K, V]:
-    from a_sync.task import TaskMapping
-    return TaskMapping(coro_fn, *iterables, **coro_fn_kwargs)
