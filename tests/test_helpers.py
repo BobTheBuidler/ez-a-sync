15edb64cec907084fd60555013bf23e6ecf1c2d0
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-02-15 14:09:18 -0500

    feat(test): test get_event_loop (#41)
    * feat(test): test get_event_loop
    
    * fix(test): missing import

---
+++ tests/test_helpers.py
@@ -0,0 +1,13 @@
+import asyncio
+
+from a_sync._helpers import get_event_loop
+
+
+def test_get_event_loop():
+    assert get_event_loop() == asyncio.get_event_loop()
+
+def test_get_event_loop_in_thread():
+    def task():
+        assert get_event_loop() == asyncio.get_event_loop()
+    loop = get_event_loop()
+    loop.run_until_complete(loop.run_in_executor(None, task))
