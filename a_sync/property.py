d61a7e521af40aa2c6cc92676e029a17aff75e13
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-02-27 22:01:03 +0000

    fix: syntax

--- a_sync/property.py
+++ a_sync/property.py
@@ -8,7 +8,7 @@ from a_sync._typing import *
 
 
 class _ASyncPropertyDescriptorBase(ASyncDescriptor[T]):
-    _fget: UnboundMethod[ASyncInstance, [], T]
+    _fget: HiddenMethod[ASyncInstance, T]
     def __init__(self, _fget: Callable[Concatenate[ASyncInstance, P], Awaitable[T]] | Callable[Concatenate[ASyncInstance, P], T], field_name=None, **modifiers: config.ModifierKwargs):
         super().__init__(_fget, field_name, **modifiers)
         self.hidden_method_name = f"__{self.field_name}__"
