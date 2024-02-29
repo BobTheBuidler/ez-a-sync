279620f90bd91aa17ef94b650a79d712208c6d4c
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-02-29 20:29:13 +0000

    feat: Queue.get_all and Queue.get_multi

--- a_sync/primitives/queue.py
+++ a_sync/primitives/queue.py
@@ -9,64 +9,67 @@ else:
     class _Queue(asyncio.Queue[T]):...
 
 class Queue(_Queue[T]):
-    """The only difference between an a_sync.Queue and an asyncio.Queue is that `get_nowait` can retrn multiple responses."""
-    @overload
-    async def get(self, i: int = 1, can_return_less: bool = False) -> T:
-        ...
-    @overload
-    async def get(self, i: int, can_return_less: bool = False) -> List[T]:
-        ...
-    async def get(self, i: int = 1, can_return_less: bool = False) -> Union[T, List[T]]:
+    # for type hint support
+    async def get(self) -> T:
+        return await super().get()
+    def get_nowait(self) -> T:
+        return super().get_nowait()
+    async def put(self, item: T) -> None:
+        return super().put(item)
+    def put_nowait(self, item: T) -> None:
+        return super().put_nowait(item)
+    
+    async def get_all(self) -> List[T]:
+        """returns 1 or more items"""
+        try:
+            return self.get_all_nowait()
+        except asyncio.QueueEmpty:
+            return [await self.get()]
+    def get_all_nowait(self) -> List[T]:
+        """returns 1 or more items, or raises asyncio.QueueEmpty"""
+        values: List[T] = []
+        while True:
+            try:
+                values.append(self.get_nowait())
+            except asyncio.QueueEmpty as e:
+                if not values:
+                    raise asyncio.QueueEmpty from e
+                return values
+            
+    async def get_multi(self, i: int, can_return_less: bool = False) -> List[T]:
         _validate_args(i, can_return_less)
-        if i == 1:
-            if can_return_less:
-                raise RuntimeError("you cannot use the `can_return_less` kwarg when `i` is 1")
-            return await super().get()
-        items = self.get_nowait(i, can_return_less=True)
-        if not items:
-            items = [await super().get()]
-        if len(items) == i or can_return_less:
-            return items
-        while len(items) < i:
-            items.extend(await self.get(i - len(items)))
+        items = []
+        while len(items) < i and not can_return_less:
+            try:
+                items.extend(self.get_multi_nowait(i - len(items), can_return_less=True))
+            except asyncio.QueueEmpty:
+                items = [await self.get()]
         return items
-        
-    @overload
-    def get_nowait(self, i: int = 1, can_return_less: bool = False) -> T:
-        ...
-    @overload
-    def get_nowait(self, i: int, can_return_less: bool = False) -> List[T]:
-        ...
-    def get_nowait(self, i: int = 1, can_return_less: bool = False) -> Union[T, List[T]]:
+    def get_multi_nowait(self, i: int, can_return_less: bool = False) -> List[T]:
         """
         Just like `asyncio.Queue.get_nowait`, but will return `i` items instead of 1.
         Set `can_return_less` to True if you want to receive up to `i` items.
         """
         _validate_args(i, can_return_less)
-        values = []
-        if i == -1:
-            while True:
-                try:
-                    values.append(super().get_nowait())
-                except asyncio.QueueEmpty:
-                    return values
+        items = []
         for _ in range(i):
             try:
-                values.append(super().get_nowait())
+                items.append(self.get_nowait())
             except asyncio.QueueEmpty:
-                if can_return_less:
-                    break
-                for value in values:
+                if items and can_return_less:
+                    return items
+                # put these back in the queue since we didn't return them
+                for value in items:
                     self.put_nowait(value)
-                raise
-        return values[0] if i == 1 else values
+                raise asyncio.QueueEmpty from None
+        return items
+    
+        
 
 def _validate_args(i: int, can_return_less: bool) -> None:
     if not isinstance(i, int):
-        raise TypeError(f"`i` must be a non-zero integer. You passed {i}")
+        raise TypeError(f"`i` must be an integer greater than 1. You passed {i}")
     if not isinstance(can_return_less, bool):
         raise TypeError(f"`can_return_less` must be boolean. You passed {can_return_less}")
-    if i == 0:
-        raise ValueError(f"`i` must be a non-zero integer. You passed {i}")
-    if can_return_less and i == 1:
-        raise ValueError("you can't set i == 1 with can_return_less == True")
+    if i <= 1:
+        raise ValueError(f"`i` must be an integer greater than 1. You passed {i}")
