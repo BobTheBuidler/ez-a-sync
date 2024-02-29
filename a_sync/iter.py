de75b9e2f87d5a05224621019eca4ee0dba7cc12
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-02-29 20:30:18 +0000

    chore: refactor iter module

--- a_sync/iter.py
+++ a_sync/iter.py
@@ -1,37 +1,60 @@
 
 import asyncio
+import functools
+import inspect
+import logging
 from a_sync._typing import *
 
 
+logger = logging.getLogger(__name__)
+
 class ASyncIterable(AsyncIterable[T], Iterable[T]):
-    """An abstract iterable implementation that can be used in both a `for` loop and an `async for` loop."""
+    """A hybrid Iterable/AsyncIterable implementation that can be used in both a `for` loop and an `async for` loop."""
     def __iter__(self) -> Iterator[T]:
-        yield from ASyncIterator.wrap(self.__aiter__())
+        yield from ASyncIterator(self.__aiter__())
     @classmethod
-    def wrap(self, aiterable: AsyncIterable[T]) -> "ASyncWrappedIterable[T]":
-        return ASyncWrappedIterable(aiterable)
+    def wrap(cls, wrapped: AsyncIterable[T]) -> "ASyncIterable[T]":
+        # NOTE: for backward-compatability only. Will be removed soon.
+        logger.warning("ASyncIterable.wrap will be removed soon. Please replace uses with simple instantiation ie `ASyncIterable(wrapped)`")
+        return cls(wrapped)
+    def __init__(self, async_iterable: AsyncIterable[T]):
+        self.__wrapped__ = async_iterable
+    def __aiter__(self) -> AsyncIterator[T]:
+        return self.__wrapped__.__aiter__()
+    __slots__ = "__wrapped__", 
+
+AsyncGenFunc = Callable[P, AsyncGenerator[T, None]]
 
 class ASyncIterator(AsyncIterator[T], Iterator[T]):
-    """An abstract iterator implementation that can be used in both a `for` loop and an `async for` loop."""
+    """A hybrid Iterator/AsyncIterator implementation that can be used in both a `for` loop and an `async for` loop."""
     def __next__(self) -> T:
         try:
             return asyncio.get_event_loop().run_until_complete(self.__anext__())
         except StopAsyncIteration as e:
             raise StopIteration from e
+    @overload
+    def wrap(cls, aiterator: AsyncIterator[T]) -> "ASyncIterator[T]":...
+    @overload
+    def wrap(cls, async_gen_func: AsyncGenFunc[P, T]) -> "ASyncGeneratorFunction[P, T]":...
     @classmethod
-    def wrap(self, aiterator: AsyncIterator[T]) -> "ASyncWrappedIterator[T]":
-        return ASyncWrappedIterator(aiterator)
-
-class ASyncWrappedIterable(ASyncIterable[T]):
-    __slots__ = "__aiterable", 
-    def __init__(self, async_iterable: AsyncIterable[T]):
-        self.__aiterable = async_iterable
-    def __aiter__(self) -> AsyncIterator[T]:
-        return self.__aiterable.__aiter__()
-
-class ASyncWrappedIterator(ASyncIterator[T]):
-    __slots__ = "__aiterator", 
+    def wrap(cls, wrapped):
+        if isinstance(wrapped, AsyncIterator):
+            logger.warning("This use case for ASyncIterator.wrap will be removed soon. Please replace uses with simple instantiation ie `ASyncIterator(wrapped)`")
+            return cls(wrapped)
+        elif inspect.isasyncgenfunction(wrapped):
+            return ASyncGeneratorFunction(wrapped)
+        raise TypeError(f"`wrapped` must be an AsyncIterator or an async generator function. You passed {wrapped}")
     def __init__(self, async_iterator: AsyncIterator[T]):
-        self.__aiterator = async_iterator
+        self.__wrapped__ = async_iterator
     async def __anext__(self) -> T:
-        return await self.__aiterator.__anext__()
+        return await self.__wrapped__.__anext__()
+
+class ASyncGeneratorFunction(Generic[P, T]):
+    __slots__ = "__wrapped__", 
+    def __init__(self, async_gen_func: AsyncGenFunc[P, T]) -> None:
+        self.__wrapped__ = async_gen_func
+        functools.update_wrapper(self, self.__wrapped__)
+        assert inspect.isasyncgenfunction(self)
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> ASyncIterator[T]:
+        return ASyncIterator(self.__wrapped__(*args, **kwargs))
+    
--- tests/test_iter.py
+++ tests/test_iter.py
@@ -2,8 +2,7 @@
 
 import pytest
 
-from a_sync.iter import (ASyncIterable, ASyncIterator, ASyncWrappedIterable,
-                         ASyncWrappedIterator)
+from a_sync.iter import ASyncIterable, ASyncIterator
 
 
 async def async_gen():
@@ -12,16 +11,20 @@ async def async_gen():
     yield 2
 
 def test_iterable_wrap():
-    assert isinstance(ASyncIterable.wrap(async_gen()), ASyncWrappedIterable)
+    assert isinstance(ASyncIterable(async_gen()), ASyncIterable)
+    assert isinstance(ASyncIterable.wrap(async_gen()), ASyncIterable)
 
 def test_iterator_wrap():
-    assert isinstance(ASyncIterator.wrap(async_gen()), ASyncWrappedIterator)
+    assert isinstance(ASyncIterator(async_gen()), ASyncIterator)
+    assert isinstance(ASyncIterator.wrap(async_gen()), ASyncIterator)
 
 def test_iterable_sync():
+    assert [i for i in ASyncIterable(async_gen())] == [0, 1, 2]
     assert [i for i in ASyncIterable.wrap(async_gen())] == [0, 1, 2]
 
 @pytest.mark.asyncio_cooperative
 async def test_iterable_async():
+    assert [i async for i in ASyncIterable(async_gen())] == [0, 1, 2]
     assert [i async for i in ASyncIterable.wrap(async_gen())] == [0, 1, 2]
 
 def test_iterator_sync():
