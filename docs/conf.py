8ecdc42aa5193090f96ac1c8b61c90c6bb47fa75
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-03-30 16:50:52 -0400

    feat(docs): separate class signature (#180)

--- docs/conf.py
+++ docs/conf.py
@@ -32,13 +32,16 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-autodoc_typehints = "description"
 autodoc_default_options = {
     'private-members': True,
     'special-members': '__init__,__call__,__getitem__',
     # hide private methods that aren't relevant to us here
     'exclude-members': '_abc_impl,_fget,_fset,_fdel,_ASyncSingletonMeta__instances,_ASyncSingletonMeta__lock'
 }
+autodoc_typehints = "description"
+# Don't show class signature with the class' name.
+autodoc_class_signature = "separated"
+
 automodule_generate_module_stub = True
 
 sys.path.insert(0, os.path.abspath('./a_sync'))
