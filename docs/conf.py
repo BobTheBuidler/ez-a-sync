ac8e9813cd5b3b08653860fd3caedba6812d2b23
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-03-31 01:03:29 -0400

    feat(docs): link to external docs for py builtin types (#185)

--- docs/conf.py
+++ docs/conf.py
@@ -19,12 +19,15 @@ author = 'BobTheBuidler'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
