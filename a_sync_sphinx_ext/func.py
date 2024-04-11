807abfd5f1022e1f799b653506a7f8ca44b08d34
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-04-11 19:44:26 +0000

    feat: descriptor handling

--- a_sync_sphinx_ext/func.py
+++ a_sync_sphinx_ext/func.py
@@ -21,6 +21,7 @@ then you can change the ``a_sync_function_prefix`` configuration value:
 .. code-block:: python
 
     a_sync_function_prefix = '(function)'  # < default
+    a_sync_descriptor_prefix = '(descriptor)'  # < default
 
 With the extension installed `autodoc` will automatically find
 ASyncFunction objects (e.g. when using the automodule directive)
@@ -36,6 +37,7 @@ from docutils import nodes
 from sphinx.domains.python import PyFunction
 from sphinx.ext.autodoc import FunctionDocumenter
 
+from a_sync._descriptor import ASyncDescriptor
 from a_sync.modified import ASyncFunction
 
 
@@ -77,12 +79,53 @@ class ASyncFunctionDirective(PyFunction):
     """Sphinx task directive."""
 
     def get_signature_prefix(self, sig):
-        return [nodes.Text(self.env.config.celery_task_prefix)]
+        return [nodes.Text(self.env.config.a_sync_function_prefix)]
+
+
+
+class ASyncDescriptorDocumenter(FunctionDocumenter):
+    """Document ASyncDescriptor instance definitions."""
+
+    objtype = 'descriptor'
+    #member_order = 11
+
+    @classmethod
+    def can_document_member(cls, member, membername, isattr, parent):
+        return isinstance(member, ASyncDescriptor) and getattr(member, '__wrapped__')
+
+    def format_args(self):
+        wrapped = getattr(self.object, '__wrapped__', None)
+        if wrapped is not None:
+            sig = signature(wrapped)
+            if "self" in sig.parameters or "cls" in sig.parameters:
+                sig = sig.replace(parameters=list(sig.parameters.values())[1:])
+            return str(sig)
+        return ''
+
+    def document_members(self, all_members=False):
+        pass
+
+    def check_module(self):
+        # Normally checks if *self.object* is really defined in the module
+        # given by *self.modname*. But since functions decorated with the @task
+        # decorator are instances living in the celery.local, we have to check
+        # the wrapped function instead.
+        wrapped = getattr(self.object, '__wrapped__', None)
+        if wrapped and getattr(wrapped, '__module__') == self.modname:
+            return True
+        return super().check_module()
+
+
+class ASyncDescriptorDirective(PyFunction):
+    """Sphinx task directive."""
+
+    def get_signature_prefix(self, sig):
+        return [nodes.Text(self.env.config.a_sync_descriptor_directive)]
 
 
 def autodoc_skip_member_handler(app, what, name, obj, skip, options):
     """Handler for autodoc-skip-member event."""
-    if isinstance(obj, ASyncFunction) and getattr(obj, '__wrapped__'):
+    if isinstance(obj, (ASyncFunction, ASyncDescriptor)) and getattr(obj, '__wrapped__'):
         if skip:
             return False
     return None
@@ -91,9 +134,16 @@ def autodoc_skip_member_handler(app, what, name, obj, skip, options):
 def setup(app):
     """Setup Sphinx extension."""
     app.setup_extension('sphinx.ext.autodoc')
+    
+    
+    # function
     app.add_autodocumenter(ASyncFunctionDocumenter)
     app.add_directive_to_domain('py', 'a_sync_function', ASyncFunctionDirective)
     app.add_config_value('a_sync_function_prefix', '(function)', True)
+
+    # descriptor
+    app.add_autodocumenter(ASyncDescriptorDocumenter)
+    app.add_directive_to_domain('py', 'a_sync_descriptor', ASyncFunctionDirective)
     app.connect('autodoc-skip-member', autodoc_skip_member_handler)
 
     return {
