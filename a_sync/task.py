15efdc06dfe1f160f49055774af652ef6b695807
nvy <nic@defisafety.com>
2024-04-19 13:11:31 -0400

    fix: more fixes on task.py

--- a_sync/task.py
+++ a_sync/task.py
@@ -14,6 +14,7 @@ from a_sync.utils.iterators import as_yielded, exhaust_iterator
 logger = logging.getLogger(__name__)
 
 def create_task(coro: Awaitable[T], *, name: Optional[str] = None, skip_gc_until_done: bool = False) -> "asyncio.Task[T]":
+
     """A wrapper over `asyncio.create_task` which will work with any `Awaitable` object, not just `Coroutine` objects"""
 
     """
