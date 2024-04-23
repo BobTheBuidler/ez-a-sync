eac9398f6d5c6b12e6907172dc9b4bb7b75d2a2b
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-04-23 01:16:43 +0000

    fix(mypy): TaskMapping.pop

--- a_sync/task.py
+++ a_sync/task.py
@@ -270,7 +270,11 @@ class TaskMapping(DefaultDict[K, "asyncio.Task[V]"], AsyncIterable[Tuple[K, V]])
         self._check_empty()
         return await gather(self)
     
-    def pop(self, *args: K, cancel: bool = False) -> "Union[asyncio.Task[V], asyncio.Future[V]]":
+    @overload
+    def pop(self, item: K, cancel: bool = False) -> TaskOrFuture[V]:...
+    @overload
+    def pop(self, item: K, default: K, cancel: bool = False) -> TaskOrFuture[V]:...
+    def pop(self, *args: K, cancel: bool = False) -> TaskOrFuture[V]:
         if not cancel:
             return super().pop(*args)
         fut_or_task = super().pop(*args)
