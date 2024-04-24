61ce628791b19c22b7e274438b576eb46b72a69f
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-04-24 17:11:32 +0000

    fix: hide task destroyed logs since we expect that behavior

--- a_sync/primitives/queue.py
+++ a_sync/primitives/queue.py
@@ -135,19 +135,10 @@ class ProcessingQueue(_Queue[Tuple[P, "asyncio.Future[V]"]], Generic[P, V]):
             raise self._workers.exception()
     @functools.cached_property
     def _workers(self) -> "asyncio.Task[NoReturn]":
-        from a_sync.task import create_task
         logger.debug("starting worker task for %s", self)
-        workers = [
-            asyncio.create_task(self._worker_coro(), name=f"{self.name} [Task-{i}]") 
-            for i in range(self.num_workers)
-        ]
-        for task in workers:
-            # when this queue is garbage collected, these tasks should not log
-            task._log_destroy_pending = False
-        task = create_task(asyncio.gather(*workers), name=f"{self.name} worker main Task")
+        workers = [_start_task(self._worker_coro(), name=f"{self.name} [Task-{i}]") for i in range(self.num_workers)]
+        task = _start_task(asyncio.gather(*workers), name=f"{self.name} worker main Task")
         task._workers = workers
-        # when this queue is garbage collected, this task should not log
-        task._log_destroy_pending = False
         return task
     async def _worker_coro(self) -> NoReturn:
         args: P.args
@@ -183,6 +174,13 @@ class ProcessingQueue(_Queue[Tuple[P, "asyncio.Future[V]"]], Generic[P, V]):
         for worker in self._workers._workers:
             worker.cancel()
 
+
+def _start_task(coro: Coroutine, name: str) -> "asyncio.Task[NoReturn]":
+    from a_sync.task import create_task
+    task = asyncio.create_task(coro, name=name)
+    # when this queue is garbage collected, these tasks should not log
+    task._log_destroy_pending = False
+    return task
 
 def _validate_args(i: int, can_return_less: bool) -> None:
     if not isinstance(i, int):
