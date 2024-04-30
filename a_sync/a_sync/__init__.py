03c56344071454df45b0b6e58e3a1c0cb4ddf265
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-04-30 03:10:53 +0000

    feat: import stuff from functions module

--- a_sync/a_sync/__init__.py
+++ a_sync/a_sync/__init__.py
@@ -1,6 +1,7 @@
 
 from a_sync.a_sync.base import ASyncGenericBase
 from a_sync.a_sync.decorator import a_sync
+from a_sync.a_sync.function import ASyncFunction, ASyncFunctionAsyncDefault, ASyncFunctionSyncDefault
 from a_sync.a_sync.modifiers.semaphores import apply_semaphore
 # NOTE: Some of these we purposely import without including in __all__. Do not remove.
 from a_sync.a_sync.property import (ASyncCachedPropertyDescriptor, ASyncCachedPropertyDescriptorAsyncDefault,
@@ -14,6 +15,9 @@ __all__ = [
     # entrypoints
     "a_sync",
     "ASyncGenericBase",
+
+    # classes
+    "ASyncFunction",
 
     "property",
     "cached_property",
