4cee4e8c4ac5cfa224440edd4fb9845a07d4330c
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-09-06 03:12:52 +0000

    fix(test): fix tests on python 3.12

--- tests/test_abstract.py
+++ tests/test_abstract.py
@@ -1,8 +1,15 @@
 
+import sys
+
 import pytest
 
 from a_sync.a_sync.abstract import ASyncABC
 
+if sys.version_info >= (3, 12):
+    _MIDDLE = "without an implementation for"
+else:
+    _MIDDLE = "with abstract methods"
+
 def test_abc_direct_init():
-    with pytest.raises(TypeError, match="Can't instantiate abstract class ASyncABC with abstract methods __a_sync_default_mode__, __a_sync_flag_name__, __a_sync_flag_value__"):
+    with pytest.raises(TypeError, match=f"Can't instantiate abstract class ASyncABC {_MIDDLE} __a_sync_default_mode__, __a_sync_flag_name__, __a_sync_flag_value__"):
         ASyncABC()
