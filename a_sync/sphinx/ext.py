b99dc92f79f2f4687f7d6af28d5c27546369e58c
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-11-13 06:22:37 +0000

    chore: auto-update documentation a_sync/sphinx/ext.py

--- a_sync/sphinx/ext.py
+++ a_sync/sphinx/ext.py
@@ -1,38 +1,3 @@
-"""Sphinx documentation plugin used to document ASyncFunction instances.
-
-Introduction
-============
-
-Usage
------
-
-The ez-a-sync extension for Sphinx requires Sphinx 2.0 or later.
-
-Add the extension to your :file:`docs/conf.py` configuration module:
-
-.. code-block:: python
-
-    extensions = (...,
-                  'a_sync.sphinx.ext')
-
-If you'd like to change the prefix for tasks in reference documentation
-then you can change the ``a_sync_function_prefix`` configuration value:
-
-.. code-block:: python
-
-    a_sync_function_prefix = '(function)'  # < default
-    a_sync_descriptor_prefix = '(descriptor)'  # < default
-    a_sync_generator_function_prefix = '(genfunc)'  # < default
-
-With the extension installed `autodoc` will automatically find
-ASyncFunction objects (e.g. when using the automodule directive)
-and generate the correct (as well as add a ``(function)`` prefix),
-and you can also refer to the tasks using `:task:proj.tasks.add`
-syntax.
-
-Use ``.. autotask::`` to alternatively manually document a task.
-"""
-
 from inspect import signature
 
 from docutils import nodes
@@ -49,22 +14,41 @@ from a_sync.iter import ASyncGeneratorFunction
 
 
 class _ASyncWrapperDocumenter:
+    """Base class for documenters that handle wrapped ASync functions."""
+
     typ: type
 
     @classmethod
     def can_document_member(cls, member, membername, isattr, parent):
+        """Determine if the member can be documented by this documenter.
+
+        Args:
+            member: The member to check.
+            membername: The name of the member.
+            isattr: Boolean indicating if the member is an attribute.
+            parent: The parent object.
+
+        Returns:
+            bool: True if the member can be documented, False otherwise.
+        """
         return (
             isinstance(member, cls.typ) and getattr(member, "__wrapped__") is not None
         )
 
     def document_members(self, all_members=False):
+        """Document members of the object.
+
+        Args:
+            all_members: Boolean indicating if all members should be documented.
+        """
         pass
 
     def check_module(self):
-        # Normally checks if *self.object* is really defined in the module
-        # given by *self.modname*. But since functions decorated with the @task
-        # decorator are instances living in the celery.local, we have to check
-        # the wrapped function instead.
+        """Check if the object is defined in the expected module.
+
+        Returns:
+            bool: True if the object is defined in the expected module, False otherwise.
+        """
         wrapped = getattr(self.object, "__wrapped__", None)
         if wrapped and getattr(wrapped, "__module__") == self.modname:
             return True
@@ -72,7 +56,14 @@ class _ASyncWrapperDocumenter:
 
 
 class _ASyncFunctionDocumenter(_ASyncWrapperDocumenter, FunctionDocumenter):
+    """Documenter for ASyncFunction instances."""
+
     def format_args(self):
+        """Format the arguments of the wrapped function.
+
+        Returns:
+            str: The formatted arguments.
+        """
         wrapped = getattr(self.object, "__wrapped__", None)
         if wrapped is not None:
             sig = signature(wrapped)
@@ -83,7 +74,14 @@ class _ASyncFunctionDocumenter(_ASyncWrapperDocumenter, FunctionDocumenter):
 
 
 class _ASyncMethodDocumenter(_ASyncWrapperDocumenter, MethodDocumenter):
+    """Documenter for ASyncMethod instances."""
+
     def format_args(self):
+        """Format the arguments of the wrapped method.
+
+        Returns:
+            str: The formatted arguments.
+        """
         wrapped = getattr(self.object, "__wrapped__")
         if wrapped is not None:
             return str(signature(wrapped))
@@ -91,17 +89,29 @@ class _ASyncMethodDocumenter(_ASyncWrapperDocumenter, MethodDocumenter):
 
 
 class _ASyncDirective:
+    """Base class for ASync directives."""
+
     prefix_env: str
 
     def get_signature_prefix(self, sig):
+        """Get the signature prefix for the directive.
+
+        Args:
+            sig: The signature to process.
+
+        Returns:
+            list: A list of nodes representing the signature prefix.
+        """
         return [nodes.Text(getattr(self.env.config, self.prefix_env))]
 
 
 class _ASyncFunctionDirective(_ASyncDirective, PyFunction):
+    """Directive for ASyncFunction instances."""
     pass
 
 
 class _ASyncMethodDirective(_ASyncDirective, PyMethod):
+    """Directive for ASyncMethod instances."""
     pass
 
 
@@ -111,36 +121,39 @@ class ASyncFunctionDocumenter(_ASyncFunctionDocumenter):
     objtype = "a_sync_function"
     typ = ASyncFunction
     priority = 15
-    # member_order = 11
 
 
 class ASyncFunctionSyncDocumenter(_ASyncFunctionDocumenter):
-    """Document ASyncFunction instance definitions."""
+    """Document ASyncFunctionSyncDefault instance definitions."""
 
     objtype = "a_sync_function_sync"
     typ = ASyncFunctionSyncDefault
     priority = 14
-    # member_order = 11
 
 
 class ASyncFunctionAsyncDocumenter(_ASyncFunctionDocumenter):
-    """Document ASyncFunction instance definitions."""
+    """Document ASyncFunctionAsyncDefault instance definitions."""
 
     objtype = "a_sync_function_async"
     typ = ASyncFunctionAsyncDefault
     priority = 13
-    # member_order = 11
 
 
 class ASyncFunctionDirective(_ASyncFunctionDirective):
+    """Directive for ASyncFunction instances."""
+
     prefix_env = "a_sync_function_prefix"
 
 
 class ASyncFunctionSyncDirective(_ASyncFunctionDirective):
+    """Directive for ASyncFunctionSyncDefault instances."""
+
     prefix_env = "a_sync_function_sync_prefix"
 
 
 class ASyncFunctionAsyncDirective(_ASyncFunctionDirective):
+    """Directive for ASyncFunctionAsyncDefault instances."""
+
     prefix_env = "a_sync_function_async_prefix"
 
 
@@ -149,31 +162,41 @@ class ASyncDescriptorDocumenter(_ASyncMethodDocumenter):
 
     objtype = "a_sync_descriptor"
     typ = ASyncDescriptor
-    # member_order = 11
 
 
 class ASyncDescriptorDirective(_ASyncMethodDirective):
-    """Sphinx task directive."""
+    """Directive for ASyncDescriptor instances."""
 
     prefix_env = "a_sync_descriptor_prefix"
 
 
 class ASyncGeneratorFunctionDocumenter(_ASyncFunctionDocumenter):
-    """Document ASyncFunction instance definitions."""
+    """Document ASyncGeneratorFunction instance definitions."""
 
     objtype = "a_sync_generator_function"
     typ = ASyncGeneratorFunction
-    # member_order = 11
 
 
 class ASyncGeneratorFunctionDirective(_ASyncFunctionDirective):
-    """Sphinx task directive."""
+    """Directive for ASyncGeneratorFunction instances."""
 
     prefix_env = "a_sync_generator_function_prefix"
 
 
 def autodoc_skip_member_handler(app, what, name, obj, skip, options):
-    """Handler for autodoc-skip-member event."""
+    """Handler for autodoc-skip-member event.
+
+    Args:
+        app: The Sphinx application object.
+        what: The type of the object being documented.
+        name: The name of the object.
+        obj: The object itself.
+        skip: Boolean indicating if the member should be skipped.
+        options: The options for the autodoc directive.
+
+    Returns:
+        bool: True if the member should be skipped, False otherwise.
+    """
     if isinstance(
         obj, (ASyncFunction, ASyncDescriptor, ASyncGeneratorFunction)
     ) and getattr(obj, "__wrapped__"):
@@ -183,7 +206,14 @@ def autodoc_skip_member_handler(app, what, name, obj, skip, options):
 
 
 def setup(app):
-    """Setup Sphinx extension."""
+    """Setup Sphinx extension.
+
+    Args:
+        app: The Sphinx application object.
+
+    Returns:
+        dict: A dictionary with metadata about the extension.
+    """
     app.setup_extension("sphinx.ext.autodoc")
 
     # function
