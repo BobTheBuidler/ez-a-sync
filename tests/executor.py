ad48fea3ef711428c61aab0184f574ef8faf9f9c
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-11-13 06:22:37 +0000

    chore: auto-update documentation tests/executor.py

--- tests/executor.py
+++ tests/executor.py
@@ -1,4 +1,5 @@
 def work():
+    """Simulates a time-consuming task by sleeping for 5 seconds."""
     import time
 
     time.sleep(5)
@@ -7,8 +8,9 @@ def work():
 from a_sync import ProcessPoolExecutor
 
 
-@pytest.marks.asyncio
+@pytest.mark.asyncio
 async def test_executor():
+    """Tests the ProcessPoolExecutor by running and submitting the work function asynchronously."""
     executor = ProcessPoolExecutor(6)
     await executor.run(work())
     await executor.submit(work())
