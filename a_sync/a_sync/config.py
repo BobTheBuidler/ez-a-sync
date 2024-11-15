866a9202036520f70fe3f690fa15ea5fd33ec59f
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-11-15 13:12:28 -0400

    fix(docs): include config.py envs

--- a_sync/a_sync/config.py
+++ a_sync/a_sync/config.py
@@ -4,21 +4,21 @@ It includes functionality for setting up executors, defining default modifiers,
 and handling environment variable configurations.
 
 Environment Variables:
-    A_SYNC_EXECUTOR_TYPE: Specifies the type of executor to use. Valid values are
+    :obj:`~A_SYNC_EXECUTOR_TYPE`: Specifies the type of executor to use. Valid values are
         strings that start with 'p' for :class:`~concurrent.futures.ProcessPoolExecutor` 
         (e.g., 'processes') or 't' for :class:`~concurrent.futures.ThreadPoolExecutor` 
         (e.g., 'threads'). Defaults to 'threads'.
-    A_SYNC_EXECUTOR_VALUE: Specifies the number of workers for the executor.
+    :obj:`~A_SYNC_EXECUTOR_VALUE`: Specifies the number of workers for the executor.
         Defaults to 8.
-    A_SYNC_DEFAULT_MODE: Sets the default mode for a_sync functions if not specified.
-    A_SYNC_CACHE_TYPE: Sets the default cache type. If not specified, defaults to None.
-    A_SYNC_CACHE_TYPED: Boolean flag to determine if cache keys should consider types.
-    A_SYNC_RAM_CACHE_MAXSIZE: Sets the maximum size for the RAM cache. Defaults to -1.
-    A_SYNC_RAM_CACHE_TTL: Sets the time-to-live for cache entries. If not specified,
+    :obj:`~A_SYNC_DEFAULT_MODE`: Sets the default mode for a_sync functions if not specified.
+    :obj:`~A_SYNC_CACHE_TYPE`: Sets the default cache type. If not specified, defaults to None.
+    :obj:`~A_SYNC_CACHE_TYPED`: Boolean flag to determine if cache keys should consider types.
+    :obj:`~A_SYNC_RAM_CACHE_MAXSIZE`: Sets the maximum size for the RAM cache. Defaults to -1.
+    :obj:`~A_SYNC_RAM_CACHE_TTL`: Sets the time-to-live for cache entries. If not specified,
         defaults to 0, which is then checked against `null_modifiers["ram_cache_ttl"]` 
         to potentially set it to `None`, meaning cache entries do not expire by default.
-    A_SYNC_RUNS_PER_MINUTE: Sets the rate limit for function execution.
-    A_SYNC_SEMAPHORE: Sets the semaphore limit for function execution.
+    :obj:`~A_SYNC_RUNS_PER_MINUTE`: Sets the rate limit for function execution.
+    :obj:`~A_SYNC_SEMAPHORE`: Sets the semaphore limit for function execution.
 
 Examples:
     To set the executor type to use threads with 4 workers, set the environment variables:
@@ -36,6 +36,8 @@ Examples:
         export A_SYNC_RAM_CACHE_MAXSIZE=100
         export A_SYNC_RAM_CACHE_TTL=60
 
+TODO: explain how and where these values are used
+
 See Also:
     - :mod:`concurrent.futures`: For more details on executors.
     - :mod:`functools`: For caching mechanisms.
@@ -49,7 +51,15 @@ from concurrent.futures._base import Executor
 from a_sync._typing import *
 
 EXECUTOR_TYPE = os.environ.get("A_SYNC_EXECUTOR_TYPE", "threads")
+"""Specifies the type of executor to use.
+
+Valid values are strings that start with 'p' for :class:`~concurrent.futures.ProcessPoolExecutor`
+(e.g., 'processes') or 't' for :class:`~concurrent.futures.ThreadPoolExecutor` (e.g., 'threads').
+Defaults to 'threads'.
+"""
+
 EXECUTOR_VALUE = int(os.environ.get("A_SYNC_EXECUTOR_VALUE", 8))
+"""Specifies the number of workers for the executor. Defaults to 8."""
 
 
 @functools.lru_cache(maxsize=1)
@@ -106,29 +116,49 @@ null_modifiers = ModifierKwargs(
 # User configurable default modifiers to be applied to any a_sync decorated function if you do not specify kwarg values for each modifier.
 
 DEFAULT_MODE: DefaultMode = os.environ.get("A_SYNC_DEFAULT_MODE")  # type: ignore [assignment]
+"""Sets the default mode for a_sync functions if not specified."""
+
 CACHE_TYPE: CacheType = (
     typ
     if (typ := os.environ.get("A_SYNC_CACHE_TYPE", "").lower())
     else null_modifiers["cache_type"]
 )
+"""Sets the default cache type. If not specified, defaults to None."""
+
 CACHE_TYPED = bool(os.environ.get("A_SYNC_CACHE_TYPED"))
+"""Boolean flag to determine if cache keys should consider types."""
+
 RAM_CACHE_MAXSIZE = int(os.environ.get("A_SYNC_RAM_CACHE_MAXSIZE", -1))
+"""
+Sets the maximum size for the RAM cache. Defaults to -1.
+# TODO: explain what -1 does
+"""
+
 RAM_CACHE_TTL = (
     ttl
     if (ttl := float(os.environ.get("A_SYNC_RAM_CACHE_TTL", 0)))
     else null_modifiers["ram_cache_ttl"]
 )
+"""
+Sets the time-to-live for cache entries. If not specified, defaults to 0, which is then checked against
+`null_modifiers["ram_cache_ttl"]`  to potentially set it to `None`, meaning cache entries do not expire
+by default.
+"""
+
 
 RUNS_PER_MINUTE = (
     rpm
     if (rpm := int(os.environ.get("A_SYNC_RUNS_PER_MINUTE", 0)))
     else null_modifiers["runs_per_minute"]
 )
+"""Sets the rate limit for function execution."""
+
 SEMAPHORE = (
     rpm
     if (rpm := int(os.environ.get("A_SYNC_SEMAPHORE", 0)))
     else null_modifiers["semaphore"]
 )
+"""Sets the semaphore limit for function execution."""
 
 user_set_default_modifiers = ModifierKwargs(
     default=DEFAULT_MODE,
