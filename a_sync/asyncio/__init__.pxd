bb7229bd8d89da7c2f870e3bca231f0dd3e35c08
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-11-18 13:07:15 +0000

    fix

--- a_sync/asyncio/__init__.pxd
+++ a_sync/asyncio/__init__.pxd
@@ -1,2 +1,1 @@
 from a_sync.asyncio._create_task cimport ccreate_task, ccreate_task_simple
-__all__ = ["ccreate_task", "ccreate_task_simple"]
--- a_sync/asyncio/__init__.pyi
+++
@@ -1,6 +0,0 @@
-from a_sync.asyncio._create_task import create_task as create_task
-from a_sync.asyncio.as_completed import as_completed as as_completed
-from a_sync.asyncio.gather import gather as gather
-from a_sync.asyncio.utils import get_event_loop as get_event_loop
-
-__all__ = ['create_task', 'gather', 'as_completed', 'get_event_loop']
