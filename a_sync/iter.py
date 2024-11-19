cf6be135c7d65399315e266e00f287dc7ddc6c66
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-11-19 12:54:45 +0000

    fix: ASyncIterator docstring reformatting for subclasses

--- a_sync/iter.py
+++ a_sync/iter.py
@@ -4,6 +4,8 @@ import inspect
 import logging
 import sys
 import weakref
+from types import FunctionType
+from typing import get_args
 
 from async_property import async_cached_property
 
@@ -22,6 +24,8 @@ else:
     SortKey = SyncFn[[T], bool]
     ViewFn = AnyFn[[T], bool]
 
+
+_FORMAT_PATTERNS = ("{cls}", "{obj}")
 
 class _AwaitableAsyncIterableMixin(AsyncIterable[T]):
     """
@@ -101,19 +105,83 @@ class _AwaitableAsyncIterableMixin(AsyncIterable[T]):
         return [obj async for obj in self]
 
     def __init_subclass__(cls, **kwargs) -> None:
-        new = "When awaited, a list of all elements will be returned."
+
+        # Determine the type used for T in the subclass
+        type_argument = T  # Default value
+        type_string = ":obj:`T` objects"
+        for base in getattr(cls, '__orig_bases__', []):
+            if not hasattr(base, '__args__'):
+                continue
+
+            args = get_args(base)
+            if args and args[0] != T:
+                type_argument = args[0]
+                if not isinstance(type_argument, type):
+                    # type_argument = str(type_argument)
+                    raise NotImplementedError("I think this is prevented by the rules of python Generic", type_argument)
+                elif hasattr(type_argument, "__module__") and hasattr(type_argument, '__qualname__'):
+                    type_string = f":class:`~{type_argument.__module__}.{type_argument.__qualname__}`"
+                elif hasattr(type_argument, "__module__") and hasattr(type_argument, "__name__"):
+                    type_string = f":class:`~{type_argument.__module__}.{type_argument.__name__}`"
+                elif hasattr(type_argument, '__qualname__'):
+                    type_string = f":class:`{type_argument.__qualname__}`"
+                elif hasattr(type_argument, "__name__"):
+                    type_string = f":class:`{type_argument.__name__}`"
+                else:
+                    type_string = str(type_argument)
 
         # modify the class docstring
+        new = (
+            f"When awaited, a list of all {type_string} will be returned.\n"
+            "\n"
+            "Example:\n"
+            f"    >>> my_object = {cls.__name__}(...)\n"
+            "    >>> all_contents = await my_object\n"
+            "    >>> isinstance(all_contents, list)\n"
+            "    True\n"
+            f"    >>> isinstance(all_contents[0], {type_argument.__name__})\n"
+            "    True\n"
+        )
+
         if cls.__doc__ is None:
             cls.__doc__ = new
+        elif not cls.__doc__ or cls.__doc__.endswith('\n\n'):
+            cls.__doc__ += new
+        elif cls.__doc__.endswith('\n'):
+            cls.__doc__ += f"\n{new}"
         else:
             cls.__doc__ += f"\n\n{new}"
 
-        # format the member docstrings
-        for attr_name in dir(cls):
-            attr = getattr(cls, attr_name, None)
-            if attr is not None and attr.__doc__ and "{cls}" in attr.__doc__:
-                attr.__doc__ = attr.__doc__.replace("{cls}", f":class:`{cls.__name__}`")
+        # Update method docstrings by redefining methods
+        # This is necessary because, by default, subclasses inherit methods from their bases
+        # which means if we just update the docstring we might edit docs for unrelated objects
+        functions_to_redefine = {
+            attr_name: attr_value 
+            for attr_name in dir(cls) 
+            if isinstance(
+                attr_value:=getattr(cls, attr_name, None), 
+                FunctionType,
+            )
+            and attr_value.__doc__
+            and any(pattern in attr_value.__doc__ for pattern in _FORMAT_PATTERNS)
+        }
+
+        for function_name, function_obj in functions_to_redefine.items():
+            # Create a new function object with the docstring formatted appropriately for this class
+            redefined_function_obj = FunctionType(
+                function_obj.__code__, 
+                function_obj.__globals__,
+                name=function_obj.__name__, 
+                argdefs=function_obj.__defaults__,
+                closure=function_obj.__closure__,
+            )
+
+            redefined_function_obj.__doc__ = function_obj.__doc__.format(
+                cls=cls.__name__, 
+                obj=type_string,
+            )
+
+            setattr(cls, function_name, redefined_function_obj)
 
         return super().__init_subclass__(**kwargs)
 
--- tests/test_iter.py
+++ tests/test_iter.py
@@ -1,12 +1,9 @@
-import asyncio
-import contextlib
 import pytest
-import time
+import re
 from typing import AsyncIterator, Iterator
 
-from a_sync.a_sync.base import ASyncGenericBase
+from a_sync import ASyncIterable, ASyncIterator
 from a_sync.exceptions import SyncModeInAsyncContextError
-from a_sync.iter import ASyncIterable, ASyncIterator
 
 
 test_both = pytest.mark.parametrize("cls_to_test", [ASyncIterable, ASyncIterator])
@@ -334,3 +331,82 @@ async def test_async_iterator_aiter_method(async_generator):
     ait = ASyncIterator(async_generator())
     async_iterator = ait.__aiter__()
     assert async_iterator is ait  # Should return self
+
+@test_both
+def test_class_docstring_empty(cls_to_test):
+    """Test if the class with no docstring was correctly assigned a docstring."""
+    class SampleClassNoDocstring(cls_to_test[int]):
+        ...
+    
+    assert SampleClassNoDocstring.__doc__ == (
+        "When awaited, a list of all :class:`~builtins.int` will be returned.\n"
+        "\n"
+        "Example:\n"
+        "    >>> my_object = SampleClassNoDocstring(...)\n"
+        "    >>> all_contents = await my_object\n"
+        "    >>> isinstance(all_contents, list)\n"
+        "    True\n"
+        "    >>> isinstance(all_contents[0], int)\n"
+        "    True\n"
+    )
+
+@test_both
+def test_class_docstring_append(cls_to_test):
+    """
+    Test if the class docstring suffix is correctly formatted and appended using regular expressions.
+    """
+    class SampleClassWithDocstring(cls_to_test[int]):
+        """Sample class with a docstring."""
+
+    expected_pattern = (
+        r"Example:\n"
+        r" {4}>>> my_object = SampleClassWithDocstring\(...\)\n"
+        r" {4}>>> all_contents = await my_object\n"
+        r" {4}>>> isinstance\(all_contents, list\)\n"
+        r" {4}True\n"
+        r" {4}>>> isinstance\(all_contents\[0\], int\)\n"
+        r" {4}True\n"
+    )
+    assert re.search(expected_pattern, SampleClassWithDocstring.__doc__), f"Docstring does not match expected pattern. \nDocstring: {SampleClassWithDocstring.__doc__}"
+
+@test_both
+def test_method_docstring_replacement(cls_to_test):
+    """Test if the method docstring is correctly formatted."""
+    class SampleClass(cls_to_test[str]):
+        def sample_method(self):
+            """This method processes {obj}."""
+    
+    assert ":class:`~builtins.str`" in SampleClass.sample_method.__doc__
+
+@test_both
+def test_typevar_default(cls_to_test):
+    """Ensure that when T is unspecified, 'objects' is used."""
+    class UnspecifiedTypeClass(cls_to_test):
+        """Class dealing with {obj}."""
+        
+        def sample_method(self):
+            """This works with {obj}."""
+    
+    assert "objects" in UnspecifiedTypeClass.sample_method.__doc__
+
+@test_both
+def test_typevar_specified(cls_to_test):
+    """Ensure the typevar T reflects the specific type used."""
+    class IntClass(cls_to_test[int]):
+        """Collection of {obj}."""
+        
+        def sample_method(self):
+            """Yield {obj}."""
+    
+    assert "int" in IntClass.sample_method.__doc__
+
+@test_both
+def test_typevar_default(cls_to_test):
+    """Ensure that when T is unspecified, ':obj:`T` objects' is used."""
+    class UnspecifiedTypeClass(cls_to_test):
+        """Class dealing with {obj}."""
+        
+        def sample_method(self):
+            """This works with {obj}."""
+    
+    assert ":obj:`T` objects" in UnspecifiedTypeClass.sample_method.__doc__
