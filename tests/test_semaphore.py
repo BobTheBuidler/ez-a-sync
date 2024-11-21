620a3eb07c093d5cc0578b82299afc416e493140
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-11-21 07:53:54 -0400

    fix: accidentally made Semaphore._value unreachable (#414)

--- tests/test_semaphore.py
+++ tests/test_semaphore.py
@@ -1,5 +1,7 @@
 import pytest
 from time import time
+
+from a_sync import Semaphore
 from tests.fixtures import TestSemaphore, increment
 
 
@@ -12,15 +14,18 @@ from tests.fixtures import TestSemaphore, increment
 instance = TestSemaphore(1, sync=False)
 
 
+def test_semaphore_init():
+    assert Semaphore(1)._value == Semaphore()._value == 1
+
+
 @increment
 @pytest.mark.asyncio_cooperative
 async def test_semaphore(i: int):
     start = time()
     assert await instance.test_fn() == 1
     duration = time() - start
-    assert (
-        i < 3 or duration > i
-    )  # There is a 1 second sleep in this fn. If the semaphore is not working, all tests will complete in 1 second.
+    # There is a 1 second sleep in this fn. If the semaphore is not working, all tests will complete in 1 second.
+    assert i < 3 or duration > i 
 
 
 @increment
@@ -29,9 +34,8 @@ async def test_semaphore_property(i: int):
     start = time()
     assert await instance.test_property == 2
     duration = time() - start
-    assert (
-        i < 3 or duration > i
-    )  # There is a 1 second sleep in this fn. If the semaphore is not working, all tests will complete in 1 second.
+    # There is a 1 second sleep in this fn. If the semaphore is not working, all tests will complete in 1 second.
+    assert i < 3 or duration > i 
 
 
 @increment
@@ -43,6 +47,5 @@ async def test_semaphore_cached_property(i: int):
     # There is a 1 second sleep in this fn but a semaphore override with a value of 50.
     # You can tell it worked correctly because the class-defined semaphore value is just one, whch would cause this test to fail if it were used.
     # If the override is not working, all tests will complete in just over 1 second.
-    assert (
-        i == 1 or duration < 1.4
-    )  # We increased the threshold from 1.05 to 1.4 to help tests pass on slow github runners
+    # We increased the threshold from 1.05 to 1.4 to help tests pass on slow github runners
+    assert i == 1 or duration < 1.4
