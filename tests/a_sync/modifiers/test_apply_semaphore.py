02ae36a31b40bfcb48451940888a3d87e0b7d565
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-11-26 03:34:22 +0000

    feat(test): test for failure cases too

--- tests/a_sync/modifiers/test_apply_semaphore.py
+++ tests/a_sync/modifiers/test_apply_semaphore.py
@@ -2,6 +2,7 @@ import asyncio
 import pytest
 
 from a_sync import Semaphore, apply_semaphore
+from a_sync.exceptions import FunctionNotAsync
 
 
 @pytest.mark.asyncio_cooperative
@@ -15,3 +16,22 @@ async def test_apply_semaphore_asyncio_semaphore():
 @pytest.mark.asyncio_cooperative
 async def test_apply_semaphore_a_sync_semaphore():
     apply_semaphore(asyncio.sleep, Semaphore(1))
+
+def fail():
+    pass
+
+@pytest.mark.asyncio_cooperative
+async def test_apply_semaphore_failure_int():
+    with pytest.raises(FunctionNotAsync):
+        apply_semaphore(fail, 1)
+
+@pytest.mark.asyncio_cooperative
+async def test_apply_semaphore_failure_asyncio_semaphore():
+    with pytest.raises(FunctionNotAsync):
+        apply_semaphore(fail, asyncio.Semaphore(1))
+
+@pytest.mark.asyncio_cooperative
+async def test_apply_semaphore_failure_a_sync_semaphore():
+    with pytest.raises(FunctionNotAsync):
+        apply_semaphore(fail, Semaphore(1))
+
