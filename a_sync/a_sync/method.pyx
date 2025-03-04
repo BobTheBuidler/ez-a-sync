47c9fd1dbcc8aa87bb1e3ccc85ed09760c8fffa8
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-03-04 18:27:31 +0000

    feat: 10x method cache ttl

--- a_sync/a_sync/method.pyx
+++ a_sync/a_sync/method.pyx
@@ -29,9 +29,9 @@ if TYPE_CHECKING:
     from a_sync.a_sync.abstract import ASyncABC
 
 
-METHOD_CACHE_TTL = 300
+METHOD_CACHE_TTL = 3000
 
-cdef int _METHOD_CACHE_TTL = 300
+cdef int _METHOD_CACHE_TTL = 3000
 
 
 logger = logging.getLogger(__name__)
