497000c46f4a9273e85f23ae683f792968f14745
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-03-15 16:02:40 +0000

    feat: override async_property.async_cached_property

---
+++ a_sync/_property_cached.py
@@ -0,0 +1,115 @@
+from asyncio import Lock, iscoroutinefunction
+from collections import defaultdict
+from functools import update_wrapper, wraps
+from typing import Any, DefaultDict, Dict
+
+from a_sync._property import AwaitableOnly, AwaitableProxy
+
+
+ASYNC_PROPERTY_ATTR = '__async_property__'
+
+
+def async_cached_property(func, *args, **kwargs) -> "AsyncCachedPropertyDescriptor":
+    assert iscoroutinefunction(func), 'Can only use with async def'
+    return AsyncCachedPropertyDescriptor(func, *args, **kwargs)
+
+
+FieldName = str
+
+class AsyncCachedPropertyInstanceState:
+    def __init__(self) -> None:
+        self.cache: Dict[FieldName, Any] = {}
+        self.lock: DefaultDict[FieldName, Lock] = defaultdict(Lock)
+
+    __slots__ = 'cache', 'lock'
+
+
+class AsyncCachedPropertyDescriptor:
+    def __init__(self, _fget, _fset=None, _fdel=None, field_name=None) -> None:
+        self._fget = _fget
+        self._fset = _fset
+        self._fdel = _fdel
+        self.field_name = field_name or _fget.__name__
+
+        update_wrapper(self, _fget)
+        self._check_method_sync(_fset, 'setter')
+        self._check_method_sync(_fdel, 'deleter')
+
+    def __set_name__(self, owner, name):
+        self.field_name = name
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        cache = self.get_instance_state(instance).cache
+        if self.field_name in cache:
+            return AwaitableProxy(cache[self.field_name])
+        return AwaitableOnly(self.get_loader(instance))
+
+    def __set__(self, instance, value):
+        if self._fset is not None:
+            self._fset(instance, value)
+        self.set_cache_value(instance, value)
+
+    def __delete__(self, instance):
+        if self._fdel is not None:
+            self._fdel(instance)
+        self.del_cache_value(instance)
+
+    def setter(self, method):
+        self._check_method_name(method, 'setter')
+        return type(self)(self._fget, method, self._fdel, self.field_name)
+
+    def deleter(self, method):
+        self._check_method_name(method, 'deleter')
+        return type(self)(self._fget, self._fset, method, self.field_name)
+
+    def _check_method_name(self, method, method_type):
+        if method.__name__ != self.field_name:
+            raise AssertionError(
+                f'@{self.field_name}.{method_type} name must match property name'
+            )
+
+    def _check_method_sync(self, method, method_type):
+        if method and iscoroutinefunction(method):
+            raise AssertionError(
+                f'@{self.field_name}.{method_type} must be synchronous'
+            )
+
+    def get_instance_state(self, instance):
+        try:
+            return instance.__async_property__
+        except AttributeError:
+            state = AsyncCachedPropertyInstanceState()
+            instance.__async_property__ = state
+            return state
+
+    def get_lock(self, instance):
+        return self.get_instance_state(instance).lock[self.field_name]
+
+    def get_cache(self, instance):
+        return self.get_instance_state(instance).cache
+
+    def has_cache_value(self, instance):
+        return self.field_name in self.get_instance_state(instance).cache
+
+    def get_cache_value(self, instance):
+        return self.get_instance_state(instance).cache[self.field_name]
+
+    def set_cache_value(self, instance, value):
+        self.get_instance_state(instance).cache[self.field_name] = value
+
+    def del_cache_value(self, instance):
+        del self.get_instance_state(instance).cache[self.field_name]
+
+    def get_loader(self, instance):
+        @wraps(self._fget)
+        async def load_value():
+            async with self.get_lock(instance):
+                cache = self.get_instance_state(instance).cache
+                if self.field_name in cache:
+                    return cache[self.field_name]
+                value = await self._fget(instance)
+                self.__set__(instance, value)
+                return value
+        return load_value
