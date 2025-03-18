77c2bf7067467456660b8a1bf0ff1c28281ff5f2
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-03-18 21:23:47 +0000

    feat(test): test smart task factory

--- tests/test_smart.py
+++ tests/test_smart.py
@@ -1,15 +1,31 @@
-import asyncio
 import pytest
+from asyncio import create_task, get_event_loop, sleep
 
-from a_sync._smart import SmartTask
+from a_sync._smart import SmartTask, set_smart_task_factory
 
 
 @pytest.mark.asyncio_cooperative
 async def test_smart_task_await():
-    await SmartTask(asyncio.sleep(0.1), loop=None)
+    await SmartTask(sleep(0.1), loop=None)
 
 
 @pytest.mark.asyncio_cooperative
 async def test_smart_task_name():
-    t = SmartTask(asyncio.sleep(0.1), loop=None, name="test")
+    t = SmartTask(sleep(0.1), loop=None, name="test")
     assert t.get_name() == "test"
+
+def test_set_smart_task_factory():
+    set_smart_task_factory()
+    loop = get_event_loop()
+    async def do_stuff():
+        task = create_task(sleep(0))
+        assert isinstance(task, SmartTask)
+    loop.run_until_complete(do_stuff())
+
+def test_set_smart_task_factory_with_loop():
+    loop = get_event_loop()
+    set_smart_task_factory(loop)
+    async def do_stuff():
+        task = create_task(sleep(0))
+        assert isinstance(task, SmartTask)
+    loop.run_until_complete(do_stuff())
