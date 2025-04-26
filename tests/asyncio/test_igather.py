485112655239d1a2bf0deed12302a6abf7b6c012
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-25 22:49:29 -0400

    feat(test): test igather

---
+++ tests/asyncio/test_igather.py
@@ -0,0 +1,6 @@
+from a_sync import igather
+from a_sync.asyncio import sleep0
+
+@pytest.marks.asyncio_cooperative
+async def test_igather():
+    assert await igather(sleep0() for _ in range(10)) == [None] * 10
