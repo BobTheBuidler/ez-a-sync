bfd3e8db34ada2dbac2e200fdbc419ee55494a36
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-27 21:54:34 +0000

    feat: hyper-optimize sleep0

--- a_sync/asyncio/sleep.pyx
+++ a_sync/asyncio/sleep.pyx
@@ -1,8 +1,17 @@
 # cython: profile=False
 # cython: linetrace=False
 
-from types import coroutine
-
-@coroutine
-def sleep0():
-    yield
+cdef class sleep0:
+    cdef bint done
+    
+    cpdef sleep0 __await__(self) noexcept:
+        return self
+    
+    cpdef sleep0 __iter__(self) noexcept:
+        return self
+    
+    cpdef object __next__(self) except NULL:
+        if not self.done:
+            self.done = True
+            return None
+        raise StopIteration()
