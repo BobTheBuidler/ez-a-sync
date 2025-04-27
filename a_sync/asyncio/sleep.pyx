3e471a26798dab25fdcfd0c7c67ee4eebca0c76f
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-04-27 21:54:34 +0000

    feat: send and throw

--- a_sync/asyncio/sleep.pyx
+++ a_sync/asyncio/sleep.pyx
@@ -1,6 +1,8 @@
 # cython: profile=False
 # cython: linetrace=False
 
+cdef sleep0[:] ready = []
+
 cdef class sleep0:
     cdef bint done
     
@@ -12,5 +14,17 @@ cdef class sleep0:
     
     def __next__(self) -> None:
         if self.done:
+            self.done = False
+            ready.append(self)
             raise StopIteration
         self.done = True
+
+    # For Python 3.7+ compatibility (PEP 479)
+    def send(self, value) -> None:
+        if self.done:
+            raise StopIteration
+        self.done = True
+
+    def throw(self, typ, val=None, tb=None):
+        # Propagate StopIteration or exceptions
+        raise typ if val is None else typ(val)
