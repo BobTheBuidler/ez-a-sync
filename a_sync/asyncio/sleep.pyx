978767aff767bb163723da2220554e65c32ceab2
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-28 14:00:40 -0400

    feat: hyper-optimize sleep0 (#687)
    * feat: hyper-optimize sleep0
    
    * fix: no cdef dunders
    
    * feat: send and throw

--- a_sync/asyncio/sleep.pyx
+++ a_sync/asyncio/sleep.pyx
@@ -1,8 +1,34 @@
 # cython: profile=False
 # cython: linetrace=False
 
-from types import coroutine
+cdef class sleep0:
+    """
+    While asyncio.sleep(0) is already very well-optimized,
+    this equivalent helper consumes about 10% less compute.
+    """
 
-@coroutine
-def sleep0():
-    yield
+    cdef bint done
+
+    def __cinit__(self) -> None:
+        self.done = False
+    
+    def __await__(self) -> sleep0:
+        return self
+    
+    def __iter__(self) -> sleep0:
+        return self
+    
+    def __next__(self) -> None:
+        if self.done:
+            raise StopIteration
+        self.done = True
+
+    # For Python 3.7+ compatibility (PEP 479)
+    def send(self, value) -> None:
+        if self.done:
+            raise StopIteration
+        self.done = True
+
+    def throw(self, typ, val=None, tb=None):
+        # Propagate StopIteration or exceptions
+        raise typ if val is None else typ(val)
