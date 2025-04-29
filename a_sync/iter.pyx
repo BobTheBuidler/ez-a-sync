e6342ed5a9cee811ff1032ab7f626cb2a8e8632b
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-29 00:43:06 -0400

    feat: cdef class ASyncGeneratorFunction (#725)

--- a_sync/iter.pyx
+++ a_sync/iter.pyx
@@ -434,7 +434,8 @@ class ASyncIterator(_ASyncIterator):
             return cls
 
 
-class ASyncGeneratorFunction(Generic[P, T]):
+
+cdef class _ASyncGeneratorFunction:
     """
     Encapsulates an asynchronous generator function, providing a mechanism to use it as an asynchronous iterator with enhanced capabilities. This class wraps an async generator function, allowing it to be called with parameters and return an :class:`~ASyncIterator` object. It is particularly useful for situations where an async generator function needs to be used in a manner that is consistent with both synchronous and asynchronous execution contexts.
 
@@ -452,12 +453,12 @@ class ASyncGeneratorFunction(Generic[P, T]):
         - :class:`ASyncIterable`
     """
     
-    _cache_handle: TimerHandle
+    cdef readonly object _cache_handle
     "An asyncio handle used to pop the bound method from `instance.__dict__` 5 minutes after its last use."
     
-    __weakself__: "ref[object]" = None
+    cdef readonly object __weakself__
     "A weak reference to the instance the function is bound to, if any."
-    
+        
     def __init__(
         self, async_gen_func: AsyncGenFunc[P, T], instance: Any = None
     ) -> None:
@@ -473,18 +474,20 @@ class ASyncGeneratorFunction(Generic[P, T]):
         self.__wrapped__ = async_gen_func
         "The actual async generator function."
         
-        update_wrapper(self, self.__wrapped__)
-        if instance is not None:
-            self._cache_handle = self.__get_cache_handle(instance)
-            self.__weakself__ = ref(instance, self.__cancel_cache_handle)
-        
+        if instance is None:
+            self.__weakself__ = None
+            self._cache_handle = None
+        else:
+            self.__weakself__ = ref(instance, _ASyncGeneratorFunction.__cancel_cache_handle)
+            self._cache_handle = self._get_cache_handle(instance)
+    
     def __repr__(self) -> str:
         return "<{} for {} at {}>".format(
             type(self).__name__, 
             self.__wrapped__, 
             hex(id(self))
         )
-        
+     
     def __call__(self, *args: P.args, **kwargs: P.kwargs) -> ASyncIterator[T]:
         """
         Calls the wrapped async generator function with the given arguments and keyword arguments, returning an :class:`ASyncIterator`.
@@ -498,16 +501,21 @@ class ASyncGeneratorFunction(Generic[P, T]):
     
     def __get__(self, instance: V, owner: Type[V]) -> "ASyncGeneratorFunction[P, T]":
         "Descriptor method to make the function act like a non-data descriptor."
+        cdef _ASyncGeneratorFunction gen_func
+        cdef dict instance_dict
+        
         if instance is None:
             return self
-        cdef object gen_func
+        
+        instance_dict = instance.__dict__
+        
         try:
-            gen_func = instance.__dict__[self.field_name]
+            gen_func = instance_dict[self.field_name]
         except KeyError:
             gen_func = ASyncGeneratorFunction(self.__wrapped__, instance)
-            instance.__dict__[self.field_name] = gen_func
-        cancel_handle(gen_func._cache_handle)
-        gen_func._cache_handle = self.__get_cache_handle(instance)
+            instance_dict[self.field_name] = gen_func
+            
+        gen_func._set_cache_handle(self._get_cache_handle(instance))
         return gen_func
     
     @property
@@ -520,18 +528,28 @@ class ASyncGeneratorFunction(Generic[P, T]):
         if instance is None:
             raise ReferenceError(self)
         return instance
+
+    cdef inline void _set_cache_handle(self, object handle):
+        self.__cancel_cache_handle()
+        self._cache_handle = handle
     
-    def __get_cache_handle(self, instance: object) -> TimerHandle:
+    cdef inline object _get_cache_handle(self, object instance):
         # NOTE: we create a strong reference to instance here. I'm not sure if this is good or not but its necessary for now.
         return get_event_loop().call_later(
             300, delattr, instance, self.field_name
         )
-        
-    def __cancel_cache_handle(self, instance: object) -> None:
+
+    cpdef void __cancel_cache_handle(self):
         cancel_handle(self._cache_handle)
 
-        
-cdef object _ASyncGeneratorFunction = ASyncGeneratorFunction
+
+class ASyncGeneratorFunction(_ASyncGeneratorFunction, Generic[P, T]):
+    def __init__(
+        self, async_gen_func: AsyncGenFunc[P, T], instance: Any = None
+    ) -> None:
+        _ASyncGeneratorFunction.__init__(self, async_gen_func, instance)
+        update_wrapper(self, self.__wrapped__)
+    
 
 
 cdef class _ASyncView(_ASyncIterator):
