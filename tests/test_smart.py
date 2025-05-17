a6876f5433a03361162178718397018b43400236
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-17 00:00:05 -0400

    feat(test): failing case for cancelled SmartFuture (#761)

--- tests/test_smart.py
+++ tests/test_smart.py
@@ -1,12 +1,74 @@
 import pytest
+import weakref
 from asyncio import CancelledError, create_task, get_event_loop, sleep
 
-from a_sync._smart import SmartTask, set_smart_task_factory, shield, smart_task_factory
+from a_sync._smart import SmartFuture, SmartTask, set_smart_task_factory, shield, smart_task_factory
+
+
+@pytest.mark.asyncio_cooperative
+async def test_smart_future_init_no_args():
+    fut = SmartFuture()
+    assert fut._loop is get_event_loop()
+    assert fut._queue is None
+    assert fut._key is None
+    assert isinstance(fut._waiters, weakref.WeakSet)
+    assert len(fut._waiters) == 0
+
+
+@pytest.mark.asyncio_cooperative
+async def test_smart_future_init_loop_arg():
+    loop = get_event_loop()
+    fut = SmartFuture(loop=loop)
+    assert fut._loop is loop
+    assert fut._queue is None
+    assert fut._key is None
+    assert isinstance(fut._waiters, weakref.WeakSet)
+    assert len(fut._waiters) == 0
+
+
+@pytest.mark.asyncio_cooperative
+async def test_smart_future_await():
+    fut = SmartFuture()
+    get_event_loop().call_soon(fut.set_result, None)
+    await fut
+
+
+@pytest.mark.asyncio_cooperative
+async def test_smart_future_await_exc():
+    fut = SmartFuture()
+    get_event_loop().call_soon(fut.set_exception, ValueError("test"))
+    with pytest.raises(ValueError, match="test"):
+        await fut
+
+
+@pytest.mark.asyncio_cooperative
+async def test_smart_future_await_cancelled():
+    fut = SmartFuture()
+    get_event_loop().call_soon(fut.cancel)
+    with pytest.raises(CancelledError):
+        await fut
 
 
 @pytest.mark.asyncio_cooperative
 async def test_smart_task_await():
     await SmartTask(sleep(0.1), loop=None)
+
+
+@pytest.mark.asyncio_cooperative
+async def test_smart_task_await_exc():
+    async def raise_exc():
+        raise ValueError("test")
+
+    with pytest.raises(ValueError, match="test"):
+        await SmartTask(raise_exc(), loop=None)
+
+
+@pytest.mark.asyncio_cooperative
+async def test_smart_task_await_cancelled():
+    task = SmartTask(sleep(0.1), loop=None)
+    get_event_loop().call_soon(task.cancel)
+    with pytest.raises(CancelledError):
+        await task
 
 
 @pytest.mark.asyncio_cooperative
@@ -45,11 +107,11 @@ async def test_shield():
 @pytest.mark.asyncio_cooperative
 async def test_shield_exc():
     async def raise_exc():
-        raise ValueError
+        raise ValueError("test")
 
     task = create_task(raise_exc())
     shielded = shield(task)
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match="test"):
         await shielded
 
 
@@ -58,6 +120,8 @@ async def test_shield_cancel_inner():
     task = create_task(sleep(1))
     shielded = shield(task)
     task.cancel()
+    with pytest.raises(CancelledError):
+        await task
     with pytest.raises(CancelledError):
         await shielded
 
@@ -70,3 +134,5 @@ async def test_shield_cancel_outer():
     await sleep(0)
     assert not task.cancelled()
     await task
+    with pytest.raises(CancelledError):
+        await shielded
