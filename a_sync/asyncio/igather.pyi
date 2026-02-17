967c3e425eaee0d67f543349b897009eee59e481
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-02-17 03:35:22 -0400

    fix(mypy): tidy stubs and cancel message to reduce type noise

--- a_sync/asyncio/igather.pyi
+++ a_sync/asyncio/igather.pyi
@@ -1,10 +1,11 @@
 from asyncio import Future
-from typing import Awaitable, Iterable, NewType, TypeVar
+from typing import Awaitable, Iterable, TypeAlias, TypeVar
 
 __T = TypeVar("__T")
+__TGather = TypeVar("__TGather")
 
 # We can't get this from asyncio so we make our own
-_GatheringFuture = NewType("_GatheringFuture", Future[__T])
+_GatheringFuture: TypeAlias = Future[__TGather]
 
 def igather(
     coros_or_futures: Iterable[Awaitable[__T]], return_exceptions: bool = False
